00428c3cbe834f20c411f3d5d63f8d18d744319b
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-26 11:30:24 +0530

    Add files via upload

---
+++ palin11 (2).java
@@ -0,0 +1,14 @@
+import java.util.*;
+class palin11
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        String s=sc.next();
+            System.out.print(s+".");
+    }
+}
+
+
+
