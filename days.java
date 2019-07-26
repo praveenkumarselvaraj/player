303dcd8ed24fb1fd0e21a734eb8f2087846e8d5f
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-26 12:14:01 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class days {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if(s.equals("sunday") || s.equals("saturday")){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+		}
+		}
