2058f9d542392ed9db33e2c23450fc5732070981
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-26 12:11:08 +0530

    Add files via upload

---
+++ reepeat.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class reepeat {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		String s ="";
+		int c=0;
+		if(a.length() == b.length()){
+			for(int i=0;i<a.length();i++){
+				if(a.charAt(i)==b.charAt(i)){
+					s = s + a.charAt(i);
+				}
+				else{
+					c++;
+				}
+			}
+		}
+		if(s!="" && c==1){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+}
+}
+
