dbea6db649f5737fc5a7f6084ddcc039dcabc248
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-29 18:42:45 +0530

    Add files via upload

---
+++ upp (1).java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class upp {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+	s=s.replaceAll(" ","");
+	System.out.print(s);
+			
+		}
+		
+}
