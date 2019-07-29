115e9ad85fea7d32d53f0c93b82d344fc7a8978e
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-29 10:13:22 +0530

    Add files via upload

---
+++ mul11.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class mul11
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.print(a*b);
+    }
+}
